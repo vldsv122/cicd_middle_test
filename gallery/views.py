079fbfdccae583a31fcd757c52b8047854e35fb1
vldsv122 <vladsova18@knu.ua>
2023-05-23 14:44:18 +0300

    Added simple views.

--- gallery/views.py
+++ gallery/views.py
@@ -1,1 +1,11 @@
 from django.shortcuts import render, get_object_or_404
+
+
+def gallery_view(request):
+    # Return simple response
+    return render(request, 'gallery.html')
+
+def image_detail(request, pk):
+    # Return simple response
+    return render(request, 'image_detail.html')
+
