b7e7fad591190e504456984544ba97c08942bb99
vldsv122 <vladsova18@knu.ua>
2023-05-23 14:46:56 +0300

    Modified settings. Changed photo locations.

--- project_gallery/settings.py
+++ project_gallery/settings.py
@@ -119,8 +119,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+MEDIA_URL = '/photos/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'photos/')
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
